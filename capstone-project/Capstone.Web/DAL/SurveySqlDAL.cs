5498d5935bb55553e784c65ffbb88af35a7dedad
Ryan Brink <rdbrnk@gmail.com>
2017-06-29 16:26:53 -0400

    more day 2 stuff

--- capstone-project/Capstone.Web/DAL/SurveySqlDAL.cs
+++ capstone-project/Capstone.Web/DAL/SurveySqlDAL.cs
@@ -10,6 +10,15 @@ namespace Capstone.Web.DAL
 {
     public class SurveySqlDAL : ISurveyDAL
     {
+        private readonly string connectionString;
+        private const string SQL_GetSurveys = "SELECT COUNT(*) as votes FROM survey_result GROUP BY parkCode, ORDER BY votes;";
+        private const string SQL_SaveSurvey = "INSERT INTO survey_result VALUES (@parkCode, @emailAddress, @state, @activityLevel)";
+
+        public SurveySqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
         public List<Survey> GetAllSurveys()
         {
             throw new NotImplementedException();
